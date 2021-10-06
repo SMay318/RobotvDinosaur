f59b8e0d96693b8f7500df553673d1845c431e14
Sean May <s_may318@hotmail.com>
2021-10-06 15:57:29 -0400

    Hard coded the weapon in the robot file.

--- battlefield.py
+++ battlefield.py
@@ -8,25 +8,25 @@ class Battlefield:
     def run_game(self):
         pass
 
-    def display_welcome(self):
-        pass
+        def display_welcome(self):
+            pass
 
-    def battle(self):
-        pass
+        def battle(self):
+            pass
 
-    def dino_turn(self, dinosaur):
-        pass
+        def dino_turn(self, dinosaur):
+            pass
 
-    def robo_turn(self,robot):
-        pass
+        def robo_turn(self,robot):
+            pass
 
-    def show_dino_opponent_options(self):
-        pass
+        def show_dino_opponent_options(self):
+            pass
 
-    def show_robo_opponent_options(self): 
-        pass
+        def show_robo_opponent_options(self): 
+            pass
     
-    def display_winner(self):
-        pass
+        def display_winner(self):
+            pass
 
     
--- robot.py
+++ robot.py
@@ -1,16 +1,12 @@
-from dinosaur import Dinosaur
 from weapon import Weapon
 
 class Robot:
     def __init__(self, name):
         self.name = name
         self.health = 100
-        self.weapon = Weapon()
+        self.weapon = Weapon("sword", 10)
 
     def attack(self, dinosaur):
         self.weapon - self.health.dinosaur = self.health.dinosaur
         print(f"Successful hit on {dinosaur}! Health reduced to: {self.health.dinosaur}")
 
-    sword = Weapon("Sword", 10)
-    axe = Weapon("Axe", 15)
-    hammer = Weapon("Hammer", 20)
